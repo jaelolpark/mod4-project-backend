d1a02985c8432a025c699a4e355472aa7b138922
github username <github email address>
2019-07-06 16:19:56 -0400

    user controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    render json: @user
+  end 
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: {user: @user}, status: :created
+    else  
+      render json: {errors: @user.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  private 
+  def set_user
+		@user = User.find(params[:id])
+	end
+
+  def user_params
+		params.require(:user).permit(:first_name, :last_name, :username, :email, :phone_number, :address, :password, :password_confirmation)
+	end
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
 
   has_secure_password 
 
+  validates :username, {uniqeness: true}
+  validates :email, {uniqeness: true}
+
+
+
   def full_name
 		[self.first_name, ' ', self.last_name].join
   end
