b4ae3e3e42d0256de9cfce9efa6c0bc1500fcf24
github username <github email address>
2019-07-06 10:45:44 -0400

    migration files

--- backend/db/migrate/20190706063747_create_rooms.rb
+++ backend/db/migrate/20190706063747_create_rooms.rb
@@ -1,7 +1,20 @@
 class CreateRooms < ActiveRecord::Migration[5.2]
   def change
     create_table :rooms do |t|
+      t.string :name
+      t.string :slug
+      t.string :type
 
+      t.integer :price
+      t.integer :capacity
+
+      t.boolean :pets
+      t.boolean :breakfast
+      t.boolean :featured
+
+      t.text :description
+
+      t.integer :reservation_id
       t.timestamps
     end
   end
