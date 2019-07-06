0211fa26fc8e1204a6c46d44fd4252dd0e962b03
github username <github email address>
2019-07-06 10:42:48 -0400

    relation

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :rooms, through: :reservation
+  has_many :reservations
+
+  has_secure_password 
+
+  def full_name
+		[self.first_name, ' ', self.last_name].join
+  end
+  
 end
