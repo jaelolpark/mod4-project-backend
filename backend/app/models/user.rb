5e823fb67ddfba794440398da48d8d721be8c974
github username <dekadekadeka@gmail.com>
2019-07-07 16:31:23 -0400

    relationships and seeds

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
-  has_many :rooms, through: :reservation
+  # has_secure_password
+  #has to be commended out for seeding
+
   has_many :reservations
+  has_many :rooms, through: :reservations 
 
-  has_secure_password 
-
-  # validates :username, {uniqeness: true}
-  # validates :email, {uniqeness: true}
+  # validates :username, {uniqueness: true}
+  # validates :email, {uniqueness: true}
 
 
 
